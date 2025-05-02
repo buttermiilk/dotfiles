e200bc0f57a97415a95f04f818d5136afc5fc301
rin <rinbritta@gmail.com>
2025-05-02 07:34:04 +0000

    first build

---
+++ pkg.sh
@@ -0,0 +1,13 @@
+# I just don't like remembering this
+# Testing though, don't rely on it
+pacman -Sy --needed xorg-server xorg-xinit i3 alacritty dunst picom feh zsh git stow noto-fonts noto-fonts-cjk ttf-jetbrains-mono-nerd
+sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
+echo ""
+read -p "Y'testing? Can make you a user for that. (y/n) " answer
+if [["$answer" == 'y']]; then
+  useradd -m test
+  passwd test
+  echo "test ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+  su - test
+fi
+exit 0
