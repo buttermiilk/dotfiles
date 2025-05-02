e90724c2c16a67d166d1f2e39baea5c8145da336
rin <rinbritta@gmail.com>
2025-05-02 07:36:11 +0000

    forgot package

--- pkg.sh
+++ pkg.sh
@@ -1,6 +1,6 @@
 # I just don't like remembering this
 # Testing though, don't rely on it
-pacman -Sy --needed xorg-server xorg-xinit i3 alacritty dunst picom feh zsh git stow noto-fonts noto-fonts-cjk ttf-jetbrains-mono-nerd
+pacman -Sy --needed xorg-server xorg-xinit i3 i3status-rust alacritty dunst picom feh zsh git stow noto-fonts noto-fonts-cjk ttf-jetbrains-mono-nerd
 sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
 echo ""
 read -p "Y'testing? Can make you a user for that. (y/n) " answer
